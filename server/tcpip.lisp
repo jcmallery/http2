0a47af6263d5ba6f74be12371219b62fb57dcd3c
Tomáš Zellerin <tomas@zellerin.cz>
2026-04-04 15:36:19 +0200

    Fix errors due to closed sockets not having a peer.

--- server/tcpip.lisp
+++ server/tcpip.lisp
@@ -343,12 +343,16 @@ follow later (EAGAIN)."
 (defun fd-to-ip (fd)
   (with-foreign-objects ((addr '(:struct sockaddr-in)) (len :int))
     (setf (mem-ref len :int) size-of-sockaddr-in)
-    (checked-syscall #'zerop #'getpeername fd addr len)
-    (assert (= (mem-ref len :int) size-of-sockaddr-in))
-    (with-foreign-slots ((sin-family sin-port sin-addr) addr (:struct sockaddr-in))
-      (values (format nil "~a.~a.~a.~a:~a"
-                      (ldb (byte 8 0) sin-addr)
-                      (ldb (byte 8 8) sin-addr)
-                      (ldb (byte 8 16) sin-addr)
-                      (ldb (byte 8 24) sin-addr)
-                      (htons sin-port))))))
+    (handler-case
+        (progn
+          (checked-syscall #'zerop #'getpeername fd addr len)
+          (assert (= (mem-ref len :int) size-of-sockaddr-in))
+          (with-foreign-slots ((sin-family sin-port sin-addr) addr (:struct sockaddr-in))
+            (values (format nil "~a.~a.~a.~a:~a"
+                            (ldb (byte 8 0) sin-addr)
+                            (ldb (byte 8 8) sin-addr)
+                            (ldb (byte 8 16) sin-addr)
+                            (ldb (byte 8 24) sin-addr)
+                            (htons sin-port)))))
+      (error ()
+        (values "Unknown" 0)))))
